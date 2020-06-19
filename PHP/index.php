3094729ae3c05615c6087bf5f80034c173309abc
Wolfgang Jose Santamaria Cordoba <santamaria.wolfgang@gmail.com>
2020-06-19 18:42:36 -0500

    Ejecutando PHP

--- PHP/index.php
+++ PHP/index.php
@@ -2,7 +2,7 @@
 require_once "Account.php";
 require_once "Car.php";
 
-print 'Holas mundo';
+print 'Hola mundo';
 $car = new Car("QWY234", new Account("Andres Jaramillo","NPI1234"));
 $car->printDataCar();
 $car2 = new Car("TRE987", new Account("Miguel Cabrera","NPI5674"));
